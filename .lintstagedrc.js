93a34e7c6ef98983965181f8c217e8c73dc15d8c
Pixelastic <tim@pixelastic.com>
2020-02-24 17:44:46 +0100

    chore(require): Replace import with require

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,4 @@
-/* eslint-disable import/no-commonjs */
-const lintStagedConfig = require('aberlaas/build/configs/lintstaged.js');
+const lintStagedConfig = require('aberlaas/lib/configs/lintstaged.js');
 module.exports = {
   ...lintStagedConfig,
 };
